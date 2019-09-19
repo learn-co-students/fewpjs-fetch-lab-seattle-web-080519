42a3e1879d0f7dc8d3e5e31fdba9014299706844
zozep <jpark041@gmail.com>
2019-09-19 09:56:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function fetchBooks() {
-
+  let url = 'https://anapioficeandfire.com/api/books'
+ 
+  return fetch(url)
+    .then(response => response.json())
+    .then(demBooks => renderBooks(demBooks))
+    .catch(function(error) {
+        console.log('problem with ya fetch operation: ', error.message)
+    });
 }
 
-function renderBooks(json) {
+function renderBooks(demBooks) {
   const main = document.querySelector('main')
-  json.forEach(book => {
+  
+  demBooks.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
